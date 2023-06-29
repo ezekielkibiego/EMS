6a3566a599d856b599186229133bccb15aa29546
kibiego <kibiezekiel@gmail.com>
2023-06-29 12:47:45 +0300

    add allowAny for the LoginView

--- emp/settings.py
+++ emp/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated.AllowAny',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
